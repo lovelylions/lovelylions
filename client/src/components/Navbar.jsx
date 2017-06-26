cdfbbc570d301e432525c371d1d28c480e785ca0
Derek Johnson <drk.w.jhnsn@gmail.com>
2017-06-26 16:06:58 -0700

    Implement static front-end react mock-up

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = (props) => (
+  <div className="nav-bar">
+    <h1>Exquisite Corpse</h1>
+    <a>myGallery</a>
+    <a>signIn</a>
+  </div>
+);
+
+export default Navbar;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import ExquisiteWriter from './components/ExquisiteWriter.jsx';
+import Navbar from './components/Navbar.jsx'
 
 class App extends React.Component {
   constructor(props) {
@@ -8,8 +10,26 @@ class App extends React.Component {
   }
 
   render() {
-    return (<div>React App Entry</div>);
+    return (<div>
+      <div className="container">
+          <ExquisiteWriter />
+          <div className="header">
+            <Navbar />
+          </div>
+          <img className="stain" src="stained.png" />
+          <div className="canvas">
+          </div>
+      </div>
+      </div>);
   }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+
+
+<div className="nav-bar">
+<h1>Exquisite Corpse</h1>
+<a>myGallery</a>
+<a>signIn</a>
+</div>
