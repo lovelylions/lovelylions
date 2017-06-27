9d34d1e34051bd3637c2d98b1b23084acd093480
Vincent La <vincentla@Vincents-MacBook-Pro.local>
2017-06-26 18:01:53 -0700

    (feat) adding in the canvas and ability to dynamically render clicks

--- client/src/drawingCanvas.jsx
+++ client/src/drawingCanvas.jsx
@@ -4,11 +4,75 @@ import React from 'react';
 class DrawingCanvas extends React.Component {
   constructor(props) {
     super(props);
+    this.width = 300;
+    this.height = 300;
+    this.clickX = [];
+    this.clickY = [];
+    this.clickDrag = [];
+    this.paint = false;
+  }
+
+  draw(event) {
+    this.paint = true;
+    console.log(event.clientX - this.offsetLeft + this.scrollLeft, event.clientY - this.offsetTop + this.scrollTop);
+    this.addToEvents(event.clientX - this.offsetLeft + this.scrollLeft, event.clientY - this.offsetTop + this.scrollTop);
+    this.redraw();
+  }
+
+  endDraw(event) {
+    this.paint = false;
+  }
+
+  drawing(event) {
+    if (this.paint) {
+      this.addToEvents(event.clientX - this.offsetLeft + this.scrollLeft, event.clientY - this.offsetTop + this.scrollTop, true);
+      this.redraw();
+    }
+  }
+
+  addToEvents(x, y, drag) {
+    this.clickX.push(x);
+    this.clickY.push(y);
+    this.clickDrag.push(drag);
+  }
+
+  redraw() {
+    this.context.clearRect(0, 0, this.width, this.height);
+    for (var i = 0; i < this.clickX.length; i++) {
+      this.context.beginPath();
+
+      this.context.strokeStyle = "#000000";
+      this.context.lineJoin = 'round';
+      this.context.lineWidth = 5;
+
+      if (this.clickDrag[i] && i) {
+        this.context.moveTo(this.clickX[i - 1], this.clickY[i - 1]);
+      } else {
+        this.context.moveTo(this.clickX[i] - 1, this.clickY[i]);
+      }
+      this.context.lineTo(this.clickX[i], this.clickY[i]);
+      this.context.closePath();
+      this.context.stroke();
+    }
+  }
+
+  componentDidMount() {
+    this.canvas = document.getElementById('canvas');
+    this.context = this.canvas.getContext('2d');
+    this.offsetLeft = this.canvas.offsetLeft;
+    this.offsetTop = this.canvas.offsetTop;
+    this.scrollLeft = document.body.scrollLeft;
+    this.scrollTop = document.body.scrollTop;
+    document.addEventListener('scroll', (event) => {
+      console.log(this.scrollLeft, this.scrollTop);
+      this.scrollLeft = document.body.scrollLeft;
+      this.scrollTop = document.body.scrollTop;
+    })
   }
 
   render () {
     return (
-      <canvas id='canvas' width='300' height='300'>
+      <canvas onMouseLeave={this.endDraw.bind(this)} onMouseMove={this.drawing.bind(this)} onMouseDown={this.draw.bind(this)} onMouseUp={this.endDraw.bind(this)} id='canvas' width={this.width} height={this.height}>
       </canvas>
       )
   }
