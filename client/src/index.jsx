e03c09df45c3a065ffb854c63f8562395530e693
Derek Johnson <drk.w.jhnsn@gmail.com>
2017-06-27 16:44:10 -0700

    Refactor and redesign front-end mock-up

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,35 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ExquisiteWriter from './components/ExquisiteWriter.jsx';
-import Navbar from './components/Navbar.jsx'
+import DrawCanvas from './components/DrawCanvas.jsx';
+import SignIn from './components/SignIn.jsx';
+import Gallery from './components/Gallery.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      currentView: <DrawCanvas />,
+      pics: [
+        {head: 'paper.png', torso: 'paper.png', legs: 'paper.png', title: 'Title', artists: ['artist1', 'artist2', 'artist3']},
+        {head: 'paper.png', torso: 'paper.png', legs: 'paper.png', title: 'Title', artists: ['artist1', 'artist2', 'artist3']},
+        {head: 'paper.png', torso: 'paper.png', legs: 'paper.png', title: 'Title', artists: ['artist1', 'artist2', 'artist3']},
+        {head: 'paper.png', torso: 'paper.png', legs: 'paper.png', title: 'Title', artists: ['artist1', 'artist2', 'artist3']},
+        {head: 'paper.png', torso: 'paper.png', legs: 'paper.png', title: 'Title', artists: ['artist1', 'artist2', 'artist3']},
+        {head: 'paper.png', torso: 'paper.png', legs: 'paper.png', title: 'Title', artists: ['artist1', 'artist2', 'artist3']}
+      ]
+    };
+    this.switch = this.switch.bind(this);
+  }
+
+  switch(e) {
+    e.preventDefault();
+    var targetVal = e.target.innerText;
+    if (targetVal === 'signIn') {
+      this.setState({currentView: <SignIn />});
+    } else if (targetVal === 'canvas') {
+      this.setState({currentView: <DrawCanvas />});
+    } else if (targetVal === 'myGallery') {
+      this.setState({currentView: <Gallery pics={this.state.pics} />});
+    }
   }
 
   render() {
     return (<div>
-      <div className="container">
+      <div className="foreground">
           <ExquisiteWriter />
-          <div className="header">
-            <Navbar />
+          <div className="container">
+            <div className="nav-bar">
+              <h1>Exquisite Corpse</h1>
+              <a href="#" onClick={this.switch}>canvas</a>
+              <a href="#" onClick={this.switch}>myGallery</a>
+              <a href="#" onClick={this.switch}>signIn</a>
+            </div>
+            {this.state.currentView}
           </div>
-          <img className="stain" src="stained.png" />
-          <div className="canvas">
-          </div>
-      </div>
+        </div>
       </div>);
   }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
-
-
-<div className="nav-bar">
-<h1>Exquisite Corpse</h1>
-<a>myGallery</a>
-<a>signIn</a>
-</div>
